fc7cec81bea671b7fb5ae241ec5b7f9c7c40921c
Chris Paulusson <chrispaulusson@chriss-mbp.clients.eduroam.liu.se>
2019-03-11 11:17:55 +0100

    update

--- Kandidaten/Assets/Resources/projectileShooter.cs
+++ Kandidaten/Assets/Resources/projectileShooter.cs
@@ -2,25 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class projectileShooter : MonoBehaviour
 {
-    GameObject prefab;
+    Rigidbody rb;
+    GameObject prefab, projectile;
     Vector3 startPos, endPos, direction;
     public float shootPower = 0.001f;
+
 
     void Start()
     {
         prefab = Resources.Load("projectile") as GameObject;
     }
 
-    // Update is called once per frame
-
     void OnMouseDown()
     {
         if (Input.GetMouseButtonDown(0))
         {
             startPos = Input.mousePosition;
-            startPos.z = 10;
+            startPos.z = 14.5f;
+            projectile = Instantiate(prefab, Camera.main.ScreenToWorldPoint(startPos), Quaternion.identity) as GameObject;
+            rb = projectile.GetComponent<Rigidbody>();
         }
     }
 
@@ -29,13 +33,11 @@ public class projectileShooter : MonoBehaviour
         if (Input.GetMouseButtonUp(0))
         {
             endPos = Input.mousePosition;
-            endPos.z = 10;
+            endPos.z = 17f;
             direction = startPos - endPos;
-
-            GameObject projectile = Instantiate(prefab, Camera.main.ScreenToWorldPoint(startPos), Quaternion.identity) as GameObject;
-            Rigidbody rb = projectile.GetComponent<Rigidbody>();
-            rb.isKinematic = false;
             rb.AddForce(direction * shootPower);
+            rb.AddForce(Vector3.forward * 10f);
         }
     }
+
 }
---
+++ Kandidaten/Assets/fort.cs
@@ -0,0 +1,48 @@
+
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class fort : MonoBehaviour {
+
+	public float startHealth;
+	public float health = 100f;
+	public float damage = 34f;
+	// Use this for initialization'
+
+	public Image HealthBar;
+
+	void Start () {
+		startHealth = health;
+	}
+
+	void Update(){
+
+	}
+
+	void TakeDamage(float amount){
+
+		health -= amount;
+		HealthBar.fillAmount = health/startHealth;
+		if(health <= 0 )
+		{
+			Die();
+		}
+
+	}
+
+	void Die()
+	{
+		Destroy(gameObject);
+	}
+
+	void OnCollisionEnter(Collision coll){
+
+		if(coll.gameObject.tag == "Player")
+		{
+			Destroy(coll.gameObject);
+			TakeDamage(damage);
+		}
+
+	}
+}
