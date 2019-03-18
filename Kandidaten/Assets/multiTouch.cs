24afe9e8651cf107cda31aa7cef08e0374269b8d
chrpa821 <chrispaulusson@gmail.com>
2019-03-18 11:05:05 +0100

    testing multi touch

---
+++ Kandidaten/Assets/multiTouch.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class multiTouch : MonoBehaviour {
+
+	public Text tCount;
+
+	// Update is called once per frame
+	void Update () {
+
+		tCount.text = Input.touchCount.ToString();
+	}
+}
