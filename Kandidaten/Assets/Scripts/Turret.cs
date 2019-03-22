80fdb4cf9831d4b68d1e65adcbac7cdce31171a8
chrpa821 <chrispaulusson@gmail.com>
2019-03-22 13:45:39 +0100

    Organized + shooting towers

---
+++ Kandidaten/Assets/Scripts/Turret.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour {
+
+	private Transform target;
+
+	[Header("Attributes")]
+	public float range = 15f;
+	public float fireRate = 1f;
+	private float fireCountDown = 0f;
+
+	[Header("Unity Setup fields")]
+	public Transform partToRotate;
+	private string enemyTag = "Enemy";
+	public float turnSpeed = 10f;
+
+	public GameObject bulletPrefab;
+	public Transform firePoint;
+
+	void Start () {
+		InvokeRepeating("UpdateTarget", 0f, 0.5f);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(target == null)
+		{
+			return;
+		}
+
+		//target to lock on
+		Vector3 dir = target.position - transform.position;
+		Quaternion lookRotation = Quaternion.LookRotation(dir);
+		Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+		partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+
+		if(fireCountDown <= 0f)
+		{
+			Shoot();
+			fireCountDown = 1f/fireRate;
+		}
+		fireCountDown -= Time.deltaTime;
+
+	}
+
+	void Shoot(){
+		Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+	}
+
+	void UpdateTarget(){
+		GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+		float shortestDistance = Mathf.Infinity;
+		GameObject nearestEnemy = null;
+
+		foreach (GameObject enemy in enemies)
+		{
+			float distToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+			if(distToEnemy < shortestDistance)
+			{
+				shortestDistance = distToEnemy;
+				nearestEnemy = enemy;
+			}
+		}
+
+		if(nearestEnemy != null && shortestDistance <= range)
+		{
+			target = nearestEnemy.transform;
+		}
+		else target = null;
+	}
+
+	void OnDrawGizmosSelected(){
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(transform.position, range);
+	}
+}
