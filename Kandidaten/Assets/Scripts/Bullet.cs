2e8911dcbaa946ae8489da715798b0f590cad9a5
chrpa821 <chrispaulusson@gmail.com>
2019-04-07 15:04:22 +0200

    chop and missileLauncher

--- Kandidaten/Assets/Scripts/Bullet.cs
+++ Kandidaten/Assets/Scripts/Bullet.cs
@@ -5,6 +5,8 @@ public class Bullet : MonoBehaviour {
 	private Transform target;
 	public float speed = 70f;
 	public float explosionRadius = 0f;
+	public GameObject impactEffect;
+
 	public void Seek(Transform _target){
 		target = _target;
 
@@ -31,9 +33,12 @@ public class Bullet : MonoBehaviour {
 	}
 
 	void HitTarget(){
+		GameObject effectIns = (GameObject) Instantiate(impactEffect,transform.position,transform.rotation);
+		Destroy(effectIns, 2f);
 		if(explosionRadius > 0f){
 			Explode();
 		}else{
+
 			Damage(target);
 		}
 		Destroy(gameObject);
