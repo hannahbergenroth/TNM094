d5c123fdc0de0f23c44898f397e4f87964d3541c
Johanna Palmqvist <johpa016@student.liu.se>
2019-04-10 14:49:57 +0200

    DestructTowers
    Ny scen med simulering för förstörda torn

---
+++ Kandidaten/Assets/Scripts/ShatterOnCollision.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShatterOnCollision : MonoBehaviour
+{
+    public GameObject replacement;
+
+    void OnCollisionEnter(Collision col)
+    {
+        //Spawn cracked version of gameobject and despawn original
+        GameObject inst = GameObject.Instantiate(replacement, transform.position + new Vector3(0f,-1.8f,0f), transform.rotation * Quaternion.Euler(90f, 0f, 0f));
+        Destroy(gameObject);
+
+        //Despawn instance after 3s
+        Destroy(inst, 3f);
+    }
+}
