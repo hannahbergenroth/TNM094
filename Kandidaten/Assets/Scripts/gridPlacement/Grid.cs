1afc4eb0cd52c5b2b2d0ec619c062b9c39da761b
Johanna Palmqvist <johpa016@student.liu.se>
2019-05-12 17:02:24 +0200

    new gridsystem + tree spawner

---
+++ Kandidaten/Assets/Scripts/gridPlacement/Grid.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Followed tutorial from https://www.youtube.com/watch?v=VBZFYGWvm4A
+*/
+
+public class Grid : MonoBehaviour
+{
+    public float gridWidth = 40;
+    public float gridHeight = 20;
+
+    [SerializeField]
+    private float size = 1f;
+
+    public Vector3 GetNearestPointOnGrid(Vector3 position)
+    {
+        position -= transform.position;
+
+        int xCount = Mathf.RoundToInt(position.x / size);
+        int yCount = Mathf.RoundToInt(position.y / size);
+        int zCount = Mathf.RoundToInt(position.z / size);
+
+        Vector3 result = new Vector3(
+            (float)xCount * size,
+            (float)yCount * size,
+            (float)zCount * size);
+
+        result += transform.position;
+
+        return result;
+    }
+
+    //for debuggin, creates a visual representation of the grid
+    /*
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        for (float z = 0; z < gridWidth; z += size)
+        {
+            for (float x = 0; x < gridHeight; x += size)
+            {
+                var point = GetNearestPointOnGrid(new Vector3(x, 0f, z));
+                Gizmos.DrawSphere(point, 0.1f);
+            }
+                
+        }
+    }
+    */
+}
---
+++ Kandidaten/Assets/Scripts/gridPlacement/ObjectPlacer.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Followed tutorial from https://www.youtube.com/watch?v=VBZFYGWvm4A
+*/
+
+public class ObjectPlacer : MonoBehaviour
+{
+    //Array of all prefabs to be spawned
+    public GameObject[] trees;
+    public float minSpawnTime = 0;
+    public float maxSpawnTime = 2;
+    public float radius = 1;
+
+    private float spawnWait;
+    private Grid grid;
+    private IEnumerator coroutine;
+
+    private void Awake()
+    {
+        grid = FindObjectOfType<Grid>();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(waitSpawner());
+    }
+
+    private void Update()
+    {
+        spawnWait = Random.Range(minSpawnTime, maxSpawnTime);
+        
+        //spawn from clicking, not to be used for trees
+        /*
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hitInfo;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                //Kolla så att inte position redan ockuperad
+                if (hitInfo.transform.tag != "Untagged")
+                {
+                    PlaceCubeNear(hitInfo.point);
+                }
+            }
+        }*/
+    }
+
+    private IEnumerator waitSpawner()
+    {
+        //yield return new WaitForSeconds(Random.Range(minSpawnTime, maxSpawnTime));
+        float radius = 1;
+
+        while(true)
+        {
+            Vector3 spawnPoint = new Vector3(Random.Range(0, grid.gridHeight-1), 0f, Random.Range(0, grid.gridWidth-1));
+            var finalPosition = grid.GetNearestPointOnGrid(spawnPoint);
+            Vector3 comparePoint = finalPosition + new Vector3(0f, 1.2f, 0f);
+            //check if object already is at position
+            if (!Physics.CheckSphere(comparePoint, radius))
+            {
+                PlaceObjectNear(finalPosition);
+            }
+            yield return new WaitForSeconds(spawnWait);
+        }
+    }
+
+    //snap to position and spawn
+    private void PlaceObjectNear(Vector3 clickPoint)
+    {
+        GameObject tree = (GameObject)Instantiate(trees[Random.Range(0,trees.Length)], clickPoint, transform.rotation);
+
+        //Create standardObject on snapped position or original
+        //GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = finalPosition;
+        //GameObject.CreatePrimitive(PrimitiveType.Sphere).transform.position = nearPoint;
+    }
+}
